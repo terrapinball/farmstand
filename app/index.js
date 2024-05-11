78991372d1e653ab1c0eb5a9864f55f52b39c5ea
Cardiaca <owendavies802@gmail.com>
2024-05-11 09:16:05 -0700

    name updates

--- app/index.js
+++ app/index.js
@@ -1,18 +1,12 @@
 import { SafeAreaView, Text } from 'react-native';
-import { ClerkProvider, SignedIn, SignedOut } from '@clerk/clerk-expo';
+// import { ClerkProvider, SignedIn, SignedOut } from '@clerk/clerk-expo';
 
 const Home = () => {
   return (
-    <ClerkProvider>
-      <SafeAreaView style={styles.container}>
-        <SignedIn>
+      <SafeAreaView >
           <Text>You are Signed in</Text>
-        </SignedIn>
-        <SignedOut>
         <Text>You are Signed out</Text>
-        </SignedOut>
       </SafeAreaView>
-    </ClerkProvider>
   )
 }
 
